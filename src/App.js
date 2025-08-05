ab28fbf575d9b8eb05acbdf8f6f091ff82914cc9
dhivyx <dhivya16112002gmail.com>
2025-08-05 12:12:40 +0530

    update project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/navbar"
+import Searchbar from "./components/searchbar"
+import Products from "./components/products"
+import Aboutus from "./components/aboutus"
+import Footer from "./components/footer"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Searchbar></Searchbar>
+            <Products></Products>
+            <Aboutus></Aboutus>
+            <Footer></Footer>
+        </div>
+    )
 }
 
-export default App;
+export default App
